b0f90b2d0f00e24a0794f4172dad57c41c25364c
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-01-06 17:00:13 +0900

    Removed unused test files.

--- test/mio_SUITE.erl
+++ test/mio_SUITE.erl
@@ -44,30 +44,33 @@ expiration(_Config) ->
     ok = memcached:disconnect(Conn).
 
 range_search(_Config) ->
-    ok.
-%%         (set m "hello" "world")
-%%         (set m "hi" "japan")
-%%         (set m "ipod" "mp3")
-%%         (test* 'get '((hello . "world")) (get m "hello"))
-%%         (test* 'get '((hello . "world")) (get m "mio:range-search" "he" "hi" "10" "asc"))
-%%         (test* 'get '((hello . "world") (hi . "japan")) (get m "mio:range-search" "he" "ipod" "10" "asc"))
-%%         (test* 'get '((hi . "japan") (hello . "world")) (get m "mio:range-search" "he" "ipod" "10" "desc"))
-%%         (test* 'get '((hi . "japan")) (get m "mio:range-search" "he" "ipod" "1" "desc"))
-%%         (delete m "hello")
-%%         (test* 'get '() (get m "hello"))
-%%         ;; expiry tests
-%%         (set m "abc" "1")
-%%         (set m "abd" "2" :exptime 1)
-%%         (set m "abe" "3")
-%%         (test* 'get '((abd . "2") (abc . "1")) (get m "mio:range-search" "ab" "abe" "2" "desc"))
-%%         (sys-sleep 1)
-%%         (test* 'get '((abc . "1")) (get m "mio:range-search" "ab" "abe" "2" "desc"))
+    {ok, Conn} = memcached:connect(?MEMCACHED_HOST, ?MEMCACHED_PORT),
+    ok = memcached:set(Conn, "1001", "Hello"),
+    ok = memcached:set(Conn, "2001", "Japan"),
+    ok = memcached:set(Conn, "3001", "World"),
+    {ok, [{"1001","Hello"}, {"2001","Japan"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "asc"]),
+    {ok, [{"2001","Japan"}, {"1001","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "desc"]),
+    {ok, [{"1001","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "1", "asc"]),
+    {ok, [{"2001","Japan"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "1", "desc"]),
+    {ok, []} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "0", "asc"]),
+    ok = memcached:disconnect(Conn).
 
+range_search_expiration(_Config) ->
+    {ok, Conn} = memcached:connect(?MEMCACHED_HOST, ?MEMCACHED_PORT),
+    ok = memcached:set(Conn, "1001", "Hello"),
+    ok = memcached:set(Conn, "2001", "Japan", 0, 1),
+    ok = memcached:set(Conn, "3001", "World"),
+    {ok, [{"1001","Hello"}, {"2001","Japan"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "asc"]),
+    {ok, [{"2001","Japan"}, {"1001","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "desc"]),
+    timer:sleep(1000),
+    {ok, [{"1001","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "asc"]),
+    {ok, [{"1001","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "desc"]),
+    ok = memcached:disconnect(Conn).
 
 all() ->
     [
      set_and_get,
      delete,
      expiration,
-     range_search
+     range_search_expiration
     ].
