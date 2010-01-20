5bd80e278419f6fd50d3e6956c3b06d59022976b
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-01-20 15:45:31 +0900

    Refactoring mio_app.erl

--- src/mio_app.erl
+++ src/mio_app.erl
@@ -1,31 +1,56 @@
+%%    Copyright (c) 2009-2010  Taro Minowa(Higepon) <higepon@users.sourceforge.jp>
+%%
+%%    Redistribution and use in source and binary forms, with or without
+%%    modification, are permitted provided that the following conditions
+%%    are met:
+%%
+%%    1. Redistributions of source code must retain the above copyright
+%%       notice, this list of conditions and the following disclaimer.
+%%
+%%    2. Redistributions in binary form must reproduce the above copyright
+%%       notice, this list of conditions and the following disclaimer in the
+%%       documentation and/or other materials provided with the distribution.
+%%
+%%    3. Neither the name of the authors nor the names of its contributors
+%%       may be used to endorse or promote products derived from this
+%%       software without specific prior written permission.
+%%
+%%    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%%    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%%    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+%%    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+%%    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%%    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+%%    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+%%    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+%%    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+%%    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+%%    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 %%%-------------------------------------------------------------------
-%%% File    : mio.erl
+%%% File    : mio_app.erl
 %%% Author  : higepon <higepon@users.sourceforge.jp>
 %%% Description : mio application
 %%%
 %%% Created : 3 Aug 2009 by higepon <higepon@users.sourceforge.jp>
 %%%-------------------------------------------------------------------
 -module(mio_app).
-
 -behaviour(application).
--import(mio).
+-include("mio.hrl").
+
+%% API
+-export([start/0, stop/0, get_env/1, get_env/2, wait_startup/2]).
+
 %% Application callbacks
 -export([start/2, stop/1]).
 
-%% For init script
--export([start/0, stop/0]).
-
-%% Utility
--export([get_env/1, get_env/2, wait_startup/2]).
-
--include("mio.hrl").
-
+%%====================================================================
+%% API
+%%====================================================================
 start() ->
     case application:start(mio) of
         ok ->
             ok;
-        {error, Reason} ->
-            io:format("FATAL application start failed ~p~n", [Reason]);
         Other ->
             io:format("FATAL application start failed ~p~n", [Other])
     end.
@@ -41,17 +66,9 @@ stop() ->
     end.
 
 
-start(_Type, _StartArgs) ->
-    supervisor:start_link({local, mio_sup}, mio_sup, []).
-
-stop(_State) ->
-    ?INFO("mio application stopped"),
-    ?PROFILER_STOP(),
-    ok.
-
 wait_startup(Host, Port) ->
     wait_startup(10, Host, Port).
-wait_startup(0, Host, Port) ->
+wait_startup(0, _Host, _Port) ->
     {error, mio_not_started};
 wait_startup(N, Host, Port) ->
     case gen_tcp:connect(Host, Port, []) of
@@ -67,12 +84,7 @@ wait_startup(N, Host, Port) ->
 
 
 get_env(Key) ->
-    case application:get_env(mio, Key) of
-        {ok, EnvValue} ->
-            {ok, EnvValue};
-        _ ->
-            ng
-    end.
+    get_env(Key, ng).
 get_env(Key, DefaultValue) ->
     case application:get_env(mio, Key) of
         {ok, EnvValue} ->
@@ -80,3 +92,12 @@ get_env(Key, DefaultValue) ->
         _ ->
             {ok, DefaultValue}
     end.
+
+
+start(_Type, _StartArgs) ->
+    supervisor:start_link({local, mio_sup}, mio_sup, []).
+
+stop(_State) ->
+    ?INFO("mio application stopped"),
+    ?PROFILER_STOP(),
+    ok.
