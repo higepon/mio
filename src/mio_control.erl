c59674953f6723958e18ec396b17392fb44a386f
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-01-20 15:52:44 +0900

    Refactoring mio_control.erl.

--- src/mio_control.erl
+++ src/mio_control.erl
@@ -1,28 +1,31 @@
-%%% Copyright (C) 2009 Higepon <higepon@users.sourceforge.jp>.
-%%% All rights reserved.
-%%%
-%%% Redistribution and use in source and binary forms, with or without
-%%% modification, are permitted provided that the following conditions
-%%% are met:
-%%%
-%%% 1. Redistributions of source code must retain the above copyright
-%%%    notice, this list of conditions and the following disclaimer.
-%%% 2. Redistributions in binary form must reproduce the above
-%%%    copyright notice, this list of conditions and the following
-%%%    disclaimer in the documentation and/or other materials provided
-%%%    with the distribution.
-%%%
-%%% THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
-%%% OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-%%% WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-%%% ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
-%%% DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-%%% DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-%%% GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-%%% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-%%% WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-%%% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-%%% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%%    Copyright (c) 2009-2010  Taro Minowa(Higepon) <higepon@users.sourceforge.jp>
+%%
+%%    Redistribution and use in source and binary forms, with or without
+%%    modification, are permitted provided that the following conditions
+%%    are met:
+%%
+%%    1. Redistributions of source code must retain the above copyright
+%%       notice, this list of conditions and the following disclaimer.
+%%
+%%    2. Redistributions in binary form must reproduce the above copyright
+%%       notice, this list of conditions and the following disclaimer in the
+%%       documentation and/or other materials provided with the distribution.
+%%
+%%    3. Neither the name of the authors nor the names of its contributors
+%%       may be used to endorse or promote products derived from this
+%%       software without specific prior written permission.
+%%
+%%    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%%    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%%    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+%%    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+%%    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%%    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+%%    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+%%    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+%%    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+%%    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+%%    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 %%%-------------------------------------------------------------------
 %%% File    : mio_control.erl
@@ -33,8 +36,13 @@
 %%%-------------------------------------------------------------------
 -module(mio_control).
 -include("mio.hrl").
+
+%% API
 -export([start/0, usage/0]).
 
+%%====================================================================
+%% API
+%%====================================================================
 start() ->
     case init:get_argument(command) of
         {ok, [["stop"|_]|_]} ->
@@ -46,6 +54,21 @@ start() ->
             halt(1)
     end.
 
+
+usage() ->
+    io:format("~nUsage:mioctl [-n <node_name>] [-c <cookie>] <command>
+
+Available commands:
+
+  stop      - stops the mio application on <node_name>
+
+"),
+    ok.
+
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
 command_stop() ->
     case init:get_argument(nodename) of
         {ok, [[NodeName|_]|_]} ->
@@ -81,14 +104,3 @@ call(Node, Module, Func, Args) ->
             ?ERRORF("Error: ~p", [Other]),
             error
     end.
-
-
-usage() ->
-    io:format("~nUsage:mioctl [-n <node_name>] [-c <cookie>] <command>
-
-Available commands:
-
-  stop      - stops the mio application on <node_name>
-
-"),
-    ok.
