a0eee7ccebd38047f687a222ddc3db5d789fb786
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-01-28 14:27:07 +0900

    Added header to mio_sup.erl

--- src/mio_sup.erl
+++ src/mio_sup.erl
@@ -1,3 +1,40 @@
+%%    Copyright (c) 2009-2010  Taro Minowa(Higepon) <higepon@users.sourceforge.jp>
+%%
+%%    Redistribution and use in source and binary forms, with or without
+%%    modification, are permitted provided that the following conditions
+%%    are met:
+%%
+%%    1. Redistributions of source code must retain the above copyright
+%%       notice, this list of conditions and the following disclaimer.
+%%
+%%    2. Redistributions in binary form must reproduce the above copyright
+%%       notice, this list of conditions and the following disclaimer in the
+%%       documentation and/or other materials provided with the distribution.
+%%
+%%    3. Neither the name of the authors nor the names of its contributors
+%%       may be used to endorse or promote products derived from this
+%%       software without specific prior written permission.
+%%
+%%    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%%    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%%    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+%%    A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+%%    OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%%    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+%%    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+%%    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+%%    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+%%    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+%%    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+%%%-------------------------------------------------------------------
+%%% File    : mio_sup.erl
+%%% Author  : higepon <higepon@users.sourceforge.jp>
+%%% Description : Supervisor
+%%%
+%%% Created : 30 Jan 2010 by higepon <higepon@users.sourceforge.jp>
+%%%-------------------------------------------------------------------
+
 -module(mio_sup).
 -behaviour(supervisor).
 -export([init/1, start_node/3, start_node/4, start_write_serializer/0, terminate_node/1]).
