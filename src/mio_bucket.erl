774d0c6201a9a9cca6c953a5d11dca49031d0f84
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-03-10 15:10:40 +0900

    Wrote deletion patterns.

--- src/mio_bucket.erl
+++ src/mio_bucket.erl
@@ -106,26 +106,77 @@
 %%      Deletion from C3 causes O4-> C3
 %%        C1-O2 | C3'-O4'-C5
 %%
-%%      Deletion from C3 causes O4-> C3
-%%        C1-O2 | C3'-O4'-C5
-
+%%      Deletion from C5 causes O4-> C5
+%%        C1-O2 | C3-O4'-C5'
 %%
-%%    (a) C-O -> C-O'
+%%    (d) C1-O2 | C3-O4*-C5
+%%      Deletion from C1. Same as (a).
 %%
-%%    (b) C1-O2 -> [O1-O2] -> C1'-O2'
-%%      Deletion from C1 caused key transfer from O2 to O1.
+%%      Deletion from C3 causes C5 to C3.
+%%        C1-O2 | C3'-O5
 %%
-%%    (c) C1-O2  | C3-O4* -> [C1-O2 | O3-O4*] -> C1-O2'-C3'
+%%      Deletion from C5.
+%%        C1-O2 | C3-O5
+%%
+%%    (e) C1-02* | C3-O4
+%%      Deletion from C1.
+%%        -> O1-O2* | C3-O4 -> C1'-O2* | O3-O4 -> C1'-O2* | C3'-O4'
+%%
+%%      Deletion from C3. Same as (a).
+%%
+%%    (f) C1-O2* | C3-O4*
+%%      This should be never appears. C1-O2*C3.
+%%
+%%    (g) C1-O2* | C3-O4-C5
+%%      Deletion from C1.
+%%        O1-O2* | C3-O4-C5 -> C1'-O2* | O3-O4-C5 -> C1'-O2* | C3'-O4'-C5
+%%
+%%      Deletion from C3 or C5 same as (c).
+%%
+%%    (h) C1-O2* | C3-O4*-C5
+%%      Deletion from C1.
+%%        O1-O2* | C3-O4*-C5 -> C1'-O2* | O3-O4*-C5 -> C1'-O2* | C3'-O5'
+%%
+%%    (i) C1-O2-C3 | C4-O5
+%%       Same as (a) and (c)
+%%
+%%    (j) C1-O2-C3 | C4-O5*
+%%      Deletion from C1 or C3. Same as (c).
+%%
+%%      Deletion from C4
+%%        C1-O2-C3 | O4-O5* -> C1-O2 | C3-O4
+%%
+%%    (k) C-O2-C3 | C4-O5-C6
+%%      Same as (c)
+%%
+%%    (l) C1-O2-C3 | C4-O5*-C6
+%%      Deletion from C1 or C3. Same as (c).
+%%
+%%      Deletion from C4.
+%%        C1-O2-C3 | O4-O5*-C6 -> C1-O2'-C3' | C4'-O5*-C6
+%%
+%%      Deletion from C6.
+%%        C1-O2-C3 | C4-O5*-O6 -> C1-O2-C3 | C4-O6
+%%
+%%    (m) C1-O2*-C3 | C4-O5
+%%      Deletion from C1.
+%%        O1-O2*-C3 | C4-O5 -> C1'-O3' | C4-O5
+%%
 %%      Deletion from C3.
+%%        C1-O2*-O3 | C4-O5 -> C1-O3 | C4-O5
 %%
-%%    (d) C1-O2*  | C3-O4* -> [C1-O2* | O3-O4*] -> C1-O3
-%%      Deletion from C3.
+%%    (n) C1-O2*-C3 | C4-O5*
+%%      Deletion from C1 or C3. Same as (m).
 %%
-%%    (e) C1-O2-C3 | C4-O5* -> [C1-O2-O3 | C4-O5*] -> C1-O2 | C3'-O4
-%%      Deletion from C3 caused key transfer from C4 to O3.
-
-
-
+%%      Deletion from C4.
+%%        C1-O2*-C3 | O4-O5* -> C1-O2* | C3-O4
+%%
+%%    (o) C1-O2*-C3 | C4-O5-C6
+%%      Deletion from C1 or C3. Same as (m)
+%%      Deletion from C4 or C6. Same as (c)
+%%
+%%    (p) C1-O2*-C3 | C4-O5*-C6
+%%      Same as (m)
 
 %%====================================================================
 %% API
