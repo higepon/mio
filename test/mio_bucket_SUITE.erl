38d9da377b67ce23bd9b8ab51418914979bb0836
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-03-17 14:57:57 +0900

    (Red) Added test for "C1-O2$-C3 Insertion to C3  : C1-O2$ | C3'-O4".

--- test/mio_bucket_SUITE.erl
+++ test/mio_bucket_SUITE.erl
@@ -408,7 +408,50 @@ insert_c_o_c_8(_Config) ->
 
 
 
-%% (f) insertion to C3 should be tested.
+%%  C1-O2$-C3
+%%      Insertion to C3  : C1-O2$ | C3'-O4
+insert_c_o_c_9(_Config) ->
+    %% setup C1-O2$-C3
+    {Left, Middle, Right} = insert_c_o_5(_Config),
+    ok = mio_bucket:insert_op(Middle, key21, value21),
+    ok = mio_bucket:insert_op(Middle, key22, value22),
+
+    c_o_c_m = mio_bucket:get_type_op(Middle),
+
+    %% insert!
+    ok = mio_bucket:insert_op(Right, key31, value31),
+
+    %% Check C1'
+    true = mio_bucket:is_full_op(Left),
+    c_o_l = mio_bucket:get_type_op(Left),
+    {ok, value0} = mio_bucket:get_op(Left, key0),
+    {ok, value1} = mio_bucket:get_op(Left, key1),
+    {ok, value2} = mio_bucket:get_op(Left, key2),
+    Middle = mio_bucket:get_right_op(Left),
+
+    %% Check O2
+    false = mio_bucket:is_full_op(Middle),
+    c_o_r = mio_bucket:get_type_op(Middle),
+    {ok, value21} = mio_bucket:get_op(Middle, key21),
+    {ok, value22} = mio_bucket:get_op(Middle, key22),
+    Right = mio_bucket:get_right_op(Middle),
+    Left = mio_bucket:get_left_op(Middle),
+
+    %% Check C3'
+    true = mio_bucket:is_full_op(Right),
+    c_o_l = mio_bucket:get_type_op(Right),
+    {ok, value3} = mio_bucket:get_op(Right, key3),
+    {ok, value31} = mio_bucket:get_op(Right, key31),
+    {ok, value4} = mio_bucket:get_op(Right, key4),
+    Middle = mio_bucket:get_left_op(Right),
+
+    %% Check O4
+    MostRight = mio_bucket:get_right_op(Right),
+    false = mio_bucket:is_full_op(MostRight),
+    {ok, value5} = mio_bucket:get_op(MostRight, key5),
+    Right = mio_bucket:get_left_op(MostRight),
+    [] = mio_bucket:get_right_op(MostRight),
+    ok.
 
 %% Helper
 setup_full_bucket(Capacity) ->
@@ -439,5 +482,6 @@ all() ->
      insert_c_o_c_5,
      insert_c_o_c_6,
      insert_c_o_c_7,
-     insert_c_o_c_8
+     insert_c_o_c_8,
+     insert_c_o_c_9
     ].
