d0d88b688b82afa057d3dfcdf46e769a0302e98d
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-03-17 15:29:30 +0900

    (Green) Revised invarant document.

--- src/mio_bucket.erl
+++ src/mio_bucket.erl
@@ -98,7 +98,7 @@
 %%    (e) C1-O2-C3
 %%      Insertion to C1 : C1'-O2'-C3
 %%      Insertion to C3 : C1-O2'-C3'
-%%      Insertion to O2 : C1-O2'-C3
+%%      Insertion to O2 : C1-O2 | C3'-O4
 %%
 %%    (f) C1-O2$-C3
 %%      Insertion to C1  : C1'-C2-C3 -> C1'-O2 | C3'-O4
