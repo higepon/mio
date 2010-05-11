9d52d20c52c94273eae770e101d61cfac11f01db
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-05-11 16:47:33 +0900

    (Green) Just added mio_bootstrap. Not used yet.

---
+++ src/mio_bootstrap.erl
@@ -0,0 +1,102 @@
+%%%-------------------------------------------------------------------
+%%% File    : mio_bootstrap.erl
+%%% Author  : higepon <higepon@users.sourceforge.jp>
+%%% Description : bootstrap server
+%%%
+%%% Created : 11 May 2010 by higepon <higepon@users.sourceforge.jp>
+%%%-------------------------------------------------------------------
+-module(mio_bootstrap).
+-include("mio.hrl").
+-behaviour(gen_server).
+
+%% API
+-export([start_link/2, get_boot_info/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-record(state, {boot_bucket, serializer}).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
+%% Description: Starts the server
+%%--------------------------------------------------------------------
+start_link(BootBucket, Serializer) ->
+    gen_server:start_link({global, ?SERVER}, ?MODULE, [BootBucket, Serializer], []).
+
+get_boot_info() ->
+    gen_server:call(?SERVER, get_boot_info).
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
+%%--------------------------------------------------------------------
+init([BootBucket, Serializer]) ->
+    {ok, #state{boot_bucket=BootBucket, serializer=Serializer}}.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call(get_boot_info, _From, State) ->
+    Reply = {State#state.boot_bucket, State#state.serializer},
+    {reply, Reply, State};
+
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
