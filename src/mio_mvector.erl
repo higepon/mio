44c96100ac6f2dd71a66c3345e6222df60a05425
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-05-19 16:02:15 +0900

    (Green) Removed unused function.

--- src/mio_mvector.erl
+++ src/mio_mvector.erl
@@ -37,7 +37,7 @@
 -module(mio_mvector).
 
 %% API
--export([make/1, eq/2, eq/3, gt/2, gt/3, get/2, generate/1]).
+-export([make/1, eq/2, eq/3, get/2, generate/1]).
 
 
 %%====================================================================
@@ -60,12 +60,6 @@ generate(MaxLevel) ->
     {A1, A2, A3} = now(),
     random:seed(A1, A2, A3),
     [random:uniform(2) - 1 || _ <- lists:duplicate(MaxLevel, 0)].
-
-gt(A, B) ->
-    A >= B.
-gt(Level, A, B) ->
-    gt(get(A, Level), get(B, Level)).
-
 
 get(MVector, Level) ->
     case Level of
