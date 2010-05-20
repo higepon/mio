776b1c78b444156eae0db6f82766f0e0260b5433
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-05-20 17:23:00 +0900

    (Green) Tidy.

--- test/mio_multiple_nodes_tests.erl
+++ test/mio_multiple_nodes_tests.erl
@@ -8,18 +8,27 @@
 -module(mio_multiple_nodes_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-start_test() ->
+start_and_kill_test() ->
     %% start first mio server
     {ok, Mio1} = mio_sup:start_first_mio(mio_sup, 11211, 3, ".", false),
     ?assertEqual(ok, mio_app:wait_startup("127.0.0.1", 11211)),
 
     %% start second mio server
     %% On this test code, Introducer node is the same node as first mio server.
-    ?assertMatch({ok, _}, mio_sup:start_second_mio(mio_sup2, node(), 11311, 3, false)),
+    {ok, Mio2} = mio_sup:start_second_mio(mio_sup2, node(), 11311, 3, false),
     ?assertEqual(ok, mio_app:wait_startup("127.0.0.1", 11311)),
-process_flag(trap_exit, true),
+
+    %% Kill them.
+    process_flag(trap_exit, true),
     exit(Mio1, normal),
+    exit(Mio2, normal),
+
+    %% Can we reuse the port?
     {ok, Mio3} = mio_sup:start_first_mio(mio_sup3, 11211, 3, ".", false),
     ?assertEqual(ok, mio_app:wait_startup("127.0.0.1", 11211)),
+    {ok, Mio4} = mio_sup:start_first_mio(mio_sup4, 11211, 3, ".", false),
+    ?assertEqual(ok, mio_app:wait_startup("127.0.0.1", 11311)),
+    exit(Mio3, normal),
+    exit(Mio4, normal).
 
-    ?debugHere.
+
