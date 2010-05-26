035bebe9d890d1a662467d63a7b8b2c7a21ce0a0
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-05-26 17:51:51 +0900

    (Refactor) range_searh becomes shorter.

--- src/mio_skip_graph.erl
+++ src/mio_skip_graph.erl
@@ -133,27 +133,40 @@ range_search_asc_op(StartBucket, Key1, Key2, Limit) when Key1 =< Key2 ->
 range_search_asc_op(_StartBucket, _Key1, _Key2, _Limit) ->
     [].
 
+range_search_asc_rec([], _Key1, _Key2, Accum, _Count, _Limit) ->
+    Accum;
 range_search_asc_rec(_Bucket, _Key1, _Key2, Accum, Count, Limit) when Count =:= Limit ->
     Accum;
 range_search_asc_rec(Bucket, Key1, Key2, Accum, Count, Limit) ->
-    KeyValues = mio_bucket:get_range_values_op(Bucket, Key1, Key2, Limit - Count),
-    case length(KeyValues) < Limit - Count of
+    {{Min, _MinEncompass}, _} = mio_bucket:get_range_op(Bucket),
+    case Key2 >= Min of
         true ->
-            case mio_bucket:get_right_op(Bucket) of
-                [] ->
-                    Accum ++ KeyValues;
-                RightBucket ->
-                    {{RMin, _RMinEncompass}, _} = mio_bucket:get_range_op(RightBucket),
-                    case Key2 >= RMin of
-                        true ->
-                            range_search_asc_rec(RightBucket, Key1, Key2, Accum ++ KeyValues, Count + length(KeyValues), Limit);
-                        _ ->
-                            Accum ++ KeyValues
-                    end
-            end;
+            KeyValues = mio_bucket:get_range_values_op(Bucket, Key1, Key2, Limit - Count),
+            RightBucket = mio_bucket:get_right_op(Bucket),
+            range_search_asc_rec(RightBucket, Key1, Key2, Accum ++ KeyValues, Count + length(KeyValues), Limit);
         _ ->
-            Accum ++ KeyValues
+            Accum
     end.
+
+
+%%     KeyValues = mio_bucket:get_range_values_op(Bucket, Key1, Key2, Limit - Count),
+%%     case length(KeyValues) < Limit - Count of
+%%         true ->
+%%             case mio_bucket:get_right_op(Bucket) of
+%%                 [] ->
+%%                     range_search_asc_rec([], Key1, Key2, Accum ++ KeyValues, Count + length(KeyValues), Limit);
+%%                 RightBucket ->
+%%                     {{RMin, _RMinEncompass}, _} = mio_bucket:get_range_op(RightBucket),
+%%                     case Key2 >= RMin of
+%%                         true ->
+%%                             range_search_asc_rec(RightBucket, Key1, Key2, Accum ++ KeyValues, Count + length(KeyValues), Limit);
+%%                         _ ->
+%%                             Accum ++ KeyValues
+%%                     end
+%%             end;
+%%         _ ->
+%%             Accum ++ KeyValues
+%%     end.
 
 
 range_search_desc_op(StartBucket, Key1, Key2, Limit) when Key1 =< Key2 ->
@@ -162,28 +175,21 @@ range_search_desc_op(StartBucket, Key1, Key2, Limit) when Key1 =< Key2 ->
 range_search_desc_op(_StartBucket, _Key1, _Key2, _Limit) ->
     [].
 
+
+range_search_desc_rec([], _Key1, _Key2, Accum, _Count, _Limit) ->
+    Accum;
 range_search_desc_rec(_Bucket, _Key1, _Key2, Accum, Count, Limit) when Count =:= Limit ->
     Accum;
 range_search_desc_rec(Bucket, Key1, Key2, Accum, Count, Limit) ->
-    KeyValues = mio_bucket:get_range_values_op(Bucket, Key2, Key1, Limit - Count),
-    case length(KeyValues) < Limit - Count of
+    {_, {Max, _MaxEncompass}} = mio_bucket:get_range_op(Bucket),
+    case Key1 =< Max of
         true ->
-            case mio_bucket:get_left_op(Bucket) of
-                [] ->
-                    Accum ++ KeyValues;
-                LeftBucket ->
-                    {_, {LMax, _}} = mio_bucket:get_range_op(LeftBucket),
-                    case Key1 =< LMax of
-                        true ->
-                            range_search_desc_rec(LeftBucket, Key1, Key2, Accum ++ KeyValues, Count + length(KeyValues), Limit);
-                        _ ->
-                            Accum ++ KeyValues
-                    end
-            end;
+            KeyValues = mio_bucket:get_range_values_op(Bucket, Key2, Key1, Limit - Count),
+            LeftBucket = mio_bucket:get_left_op(Bucket),
+            range_search_desc_rec(LeftBucket, Key1, Key2, Accum ++ KeyValues, Count + length(KeyValues), Limit);
         _ ->
-            Accum ++ KeyValues
+            Accum
     end.
-
 
 get_range(State) ->
     {{State#node.min_key, State#node.encompass_min}, {State#node.max_key, State#node.encompass_max}}.
