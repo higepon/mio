c9b51413ee6da2a423a0da98bb8c490c8e04f499
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-06-15 15:03:11 +0900

    Enabled expiration test.

--- test/mio_tests.erl
+++ test/mio_tests.erl
@@ -35,10 +35,10 @@ mio_test_() ->
       [?_test(set_and_get_alphabet_key())],
       [?_test(delete())],
       [?_test(empty())],
-%%       [?_test(expiration())],
-      [?_test(range_search())]
-%%       [?_test(range_search_alphabet())],
-%%       [?_test(range_search_expiration())]
+      [?_test(expiration())],
+      [?_test(range_search())],
+      [?_test(range_search_alphabet())],
+      [?_test(range_search_expiration())]
      ]
     }.
 
@@ -89,14 +89,13 @@ empty() ->
     ?assertEqual(C, get_start_bucket(?MEMCACHED_HOST, ?MEMCACHED_PORT)),
     ok = memcached:disconnect(Conn).
 
-%% TODO.
-%% expiration() ->
-%%     {ok, Conn} = memcached:connect(?MEMCACHED_HOST, ?MEMCACHED_PORT),
-%%     ok = memcached:set(Conn, "1234", "myvalue", 0, 1),
-%%     {ok, "myvalue"} = memcached:get(Conn, "1234"),
-%%     timer:sleep(1000),
-%%     {error, not_found} = memcached:get(Conn, "1234"),
-%%     ok = memcached:disconnect(Conn).
+expiration() ->
+    {ok, Conn} = memcached:connect(?MEMCACHED_HOST, ?MEMCACHED_PORT),
+    ok = memcached:set(Conn, "1234", "myvalue", 0, 1),
+    ?assertEqual({ok, "myvalue"}, memcached:get(Conn, "1234")),
+    timer:sleep(1000),
+    ?assertEqual({error, not_found}, memcached:get(Conn, "1234")),
+    ok = memcached:disconnect(Conn).
 
 range_search() ->
     {ok, Conn} = memcached:connect(?MEMCACHED_HOST, ?MEMCACHED_PORT),
@@ -145,27 +144,27 @@ range_search_many_buckets_test() ->
     exit(Mio1, normal).
 
 
-%% range_search_alphabet() ->
-%%     {ok, Conn} = memcached:connect(?MEMCACHED_HOST, ?MEMCACHED_PORT),
-%%     ok = memcached:set(Conn, "Higepon", "Hello"),
-%%     ok = memcached:set(Conn, "John", "Japan"),
-%%     ok = memcached:set(Conn, "Paul", "World"),
-%%     {ok, [{"Higepon","Hello"}, {"John","Japan"}]} = memcached:get_multi(Conn, ["mio:range-search", "C", "K", "10", "asc"]),
-%%     {ok, [{"John","Japan"}, {"Higepon","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "C", "K", "10", "desc"]),
-%%     {ok, [{"Higepon","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "C", "K", "1", "asc"]),
-%%     {ok, [{"John","Japan"}]} = memcached:get_multi(Conn, ["mio:range-search", "C", "K", "1", "desc"]),
-%%     {ok, []} = memcached:get_multi(Conn, ["mio:range-search", "C", "K", "0", "asc"]),
-%%     ok = memcached:disconnect(Conn).
+range_search_alphabet() ->
+    {ok, Conn} = memcached:connect(?MEMCACHED_HOST, ?MEMCACHED_PORT),
+    ok = memcached:set(Conn, "Higepon", "Hello"),
+    ok = memcached:set(Conn, "John", "Japan"),
+    ok = memcached:set(Conn, "Paul", "World"),
+    {ok, [{"Higepon","Hello"}, {"John","Japan"}]} = memcached:get_multi(Conn, ["mio:range-search", "C", "K", "10", "asc"]),
+    {ok, [{"John","Japan"}, {"Higepon","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "C", "K", "10", "desc"]),
+    {ok, [{"Higepon","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "C", "K", "1", "asc"]),
+    {ok, [{"John","Japan"}]} = memcached:get_multi(Conn, ["mio:range-search", "C", "K", "1", "desc"]),
+    {ok, []} = memcached:get_multi(Conn, ["mio:range-search", "C", "K", "0", "asc"]),
+    ok = memcached:disconnect(Conn).
 
 
-%% range_search_expiration() ->
-%%     {ok, Conn} = memcached:connect(?MEMCACHED_HOST, ?MEMCACHED_PORT),
-%%     ok = memcached:set(Conn, "1001", "Hello"),
-%%     ok = memcached:set(Conn, "2001", "Japan", 0, 1),
-%%     ok = memcached:set(Conn, "3001", "World"),
-%%     {ok, [{"1001","Hello"}, {"2001","Japan"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "asc"]),
-%%     {ok, [{"2001","Japan"}, {"1001","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "desc"]),
-%%     timer:sleep(1000),
-%%     {ok, [{"1001","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "asc"]),
-%%     {ok, [{"1001","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "desc"]),
-%%     ok = memcached:disconnect(Conn).
+range_search_expiration() ->
+    {ok, Conn} = memcached:connect(?MEMCACHED_HOST, ?MEMCACHED_PORT),
+    ok = memcached:set(Conn, "1001", "Hello"),
+    ok = memcached:set(Conn, "2001", "Japan", 0, 1),
+    ok = memcached:set(Conn, "3001", "World"),
+    {ok, [{"1001","Hello"}, {"2001","Japan"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "asc"]),
+    {ok, [{"2001","Japan"}, {"1001","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "desc"]),
+    timer:sleep(1000),
+    {ok, [{"1001","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "asc"]),
+    {ok, [{"1001","Hello"}]} = memcached:get_multi(Conn, ["mio:range-search", "1000", "3000", "10", "desc"]),
+    ok = memcached:disconnect(Conn).
