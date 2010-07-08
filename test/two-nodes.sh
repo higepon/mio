1c05c6fde962d99877efbd3d44ce587c88f321a2
Higepon (MINOWA Taro) <higepon@gmail.com>
2010-07-08 16:51:57 +0900

    Now mio and mio-ctl accepts name@FQDN.

--- test/two-nodes.sh
+++ test/two-nodes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm -f mio.log.*
-scripts/mio -d
-scripts/mio -d -n mio2 -imio1@`hostname -s`.local -p 11811
+scripts/mio -d -n mio1@localhost.local
+scripts/mio -d -n mio2@localhost.local -imio1@localhost.local -p 11811
 sleep 1
-scripts/mioctl -n mio1 stop 1>/dev/null
-scripts/mioctl -n mio2 stop 1>/dev/null
+scripts/mioctl -n mio1@localhost.local stop 1>/dev/null
+scripts/mioctl -n mio2@localhost.local stop 1>/dev/null
 grep 'Error' mio.log.1 1>/dev/null
 case $? in
     0) echo "two-nodes failed"; exit 1;;
